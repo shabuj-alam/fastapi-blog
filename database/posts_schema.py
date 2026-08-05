2d56c64606645f86507f39c50a93df2cd8e1ea13
Shabuj <shabujalam13@gmail.com>
2026-08-05 20:03:15 +0100

    imeplement coderabbit suggestions

--- database/posts_schema.py
+++ database/posts_schema.py
@@ -7,7 +7,6 @@ from database.users_schema import UserResponse
 class PostBase(BaseModel):
     title: str = Field(..., min_length=1, max_length=100)
     content: str = Field(..., min_length=1)
-    author: str = Field(..., min_length=1, max_length=50)
 
 class PostCreate(PostBase):
     user_id: int
