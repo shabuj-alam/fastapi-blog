928f946105269857a66ca6d5a93b0f939b7b9943
Shabuj <shabujalam13@gmail.com>
2026-08-06 15:47:44 +0100

    Fixed post router paths

--- routers/posts_routers.py
+++ routers/posts_routers.py
@@ -20,3 +20,121 @@ from database.posts_schema import (
     PostResponse, 
     PostUpdate
 )
+
+router = APIRouter()
+
+##POST API
+@router.get("", response_model=list[PostResponse])
+async def get_posts(db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(
+        Select(models.Post)
+        .options(selectinload(models.Post.author))
+        .order_by(models.Post.date_posted.desc())
+        )
+    posts = result.scalars().all()
+    return posts
+
+@router.get("/{post_id}", response_model=PostResponse)
+async def get_post(post_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(
+        Select(models.Post)
+        .options(selectinload(models.Post.author))
+        .where(models.Post.id == post_id)
+    )
+    post = result.scalar_one_or_none()
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Post not found"
+            )
+    return post
+
+@router.put("/{post_id}", response_model=PostResponse)
+async def update_full_post(
+    post_id: int, 
+    post_data: PostCreate,
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    result = await db.execute(Select(models.Post).where(models.Post.id == post_id))
+    post = result.scalar_one_or_none()
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Post not found"
+            )
+
+    if post_data.user_id != post.user_id:
+        result = await db.execute(Select(models.User).where(models.User.id == post_data.user_id))
+        user = result.scalar_one_or_none()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found"
+            )
+
+    post.title = post_data.title
+    post.content = post_data.content
+    post.user_id = post_data.user_id
+
+    await db.commit()
+    await db.refresh(post, attribute_names=["author"])
+    return post
+
+@router.patch("/{post_id}", response_model=PostResponse)
+async def update_partial_post(
+    post_id: int, 
+    post_data: PostUpdate,
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    result = await db.execute(Select(models.Post).where(models.Post.id == post_id))
+    post = result.scalar_one_or_none()
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Post not found"
+            )
+
+    update_data = post_data.model_dump(exclude_unset=True)
+    for field, value in update_data.items():
+        setattr(post, field, value)
+
+    await db.commit()
+    await db.refresh(post, attribute_names=["author"])
+    return post
+
+@router.post("", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostCreate, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(Select(models.User).where(models.User.id == post.user_id))
+    user = result.scalar_one_or_none()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    new_post = models.Post(
+        title=post.title,
+        content=post.content,
+        user_id=post.user_id
+    )
+
+    db.add(new_post)
+    await db.commit()
+    await db.refresh(new_post, attribute_names=["author"])
+
+    return new_post
+
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(post_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(Select(models.Post).where(models.Post.id == post_id))
+    post = result.scalar_one_or_none()
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Post not found"
+            )
+
+    db.delete(post)
+    await db.commit()
+
+    return post
